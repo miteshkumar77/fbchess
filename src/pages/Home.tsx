af9753c35b73b6d5d7040054edd355b35d89a924
miteshkumar77 <miteshkumarca@gmail.com>
2020-06-12 15:00:46 -0400

    beginning tsc migration

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,6 @@ import BoardSVG from "../components/svggen";
 const eight_arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 class Home extends Component {
-  constructor() {
-    super();
-  }
-
   render() {
     let data = Array(64);
     data.fill("11");
