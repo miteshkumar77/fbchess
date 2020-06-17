3dcf21e7da31db9f3fb24050e9a629fcf6ba17c4
miteshkumar77 <miteshkumarca@gmail.com>
2020-06-17 17:54:15 -0400

    database added

--- server/index.js
+++ server/index.js
@@ -1,14 +1,34 @@
 var app = require("express")();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
-const port = process.env.PORT || 3001;
-app.get("/", (req, res) => {
-  res.sendFioile(__dirname + "/index.html");
-});
+const mongoose = require("mongoose");
+const config = require("./config");
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-});
-http.listen(port, () => {
-  console.log("listening on *:" + port);
+const socket_runner = async () => {
+  const port = process.env.PORT || config.server_port;
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+  });
+
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+  });
+  http.listen(port, () => {
+    console.log("listening on *:" + port);
+  });
+};
+
+mongoose
+  .connect(config.db_connection_string, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to DB");
+  socket_runner();
 });
