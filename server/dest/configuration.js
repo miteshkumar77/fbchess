9f9ac7024e509d1157cb3b55601ae25124d4de34
miteshkumar77 <miteshkumarca@gmail.com>
2020-07-01 16:27:33 -0400

    ignore

--- server/dest/configuration.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.server_port = exports.db_connection_string = void 0;
-exports.db_connection_string = "mongodb+srv://fbchessDB:fbPWD@cluster0-jfvor.mongodb.net/fbchessDB?retryWrites=true&w=majority";
-exports.server_port = 3001;
-//# sourceMappingURL=configuration.js.map
